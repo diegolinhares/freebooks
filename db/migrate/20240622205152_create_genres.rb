e1a02dd2831e0915943c7667ec07f64c8ce16f10
Diego Linhares <diegomlinhares@gmail.com>
2024-06-22 20:16:25 -0300

    feat: Add missing db constraint to Genre

--- db/migrate/20240622205152_create_genres.rb
+++ db/migrate/20240622205152_create_genres.rb
@@ -1,7 +1,7 @@
 class CreateGenres < ::ActiveRecord::Migration[7.1]
   def change
     create_table :genres do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
