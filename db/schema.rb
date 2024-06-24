44b3ac688f678e0421cdf18d1bdb1cf1c9be92ec
Diego Linhares <diegomlinhares@gmail.com>
2024-06-23 23:26:39 -0300

    feat: Improve db seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_23_035653) do
+ActiveRecord::Schema[7.2].define(version: 2024_06_23_035653) do
   create_table "authors", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 ENV["FIXTURES_PATH"] = "spec/fixtures"
 
 ::Rake.application["db:fixtures:load"].invoke
+
+::Genre.rebuild_index!
+::Author.rebuild_index!
+::Book.rebuild_index!
